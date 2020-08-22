acf746a7a164c53d735dbdd6ce069198700817ed
dgerike <dennis.gerike+github@gmail.com>
2020-08-22 10:25:25 +0200

    adding a generic unit converter

--- src/Aumuc.php
+++ src/Aumuc.php
@@ -7,4 +7,19 @@ class Aumuc
     use Speed;
     use Weight;
     use Force;
+
+    /**
+     * Generic converter
+     *
+     * @param string $from convert from this unit
+     * @param float $value
+     * @param string $to convert to this unit
+     * @return float
+     */
+    public static function convert(string $from, float $value, string $to)
+    {
+        $method = $from . '2' . $to;
+
+        return self::$method($value);
+    }
 }
