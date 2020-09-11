677099bc66289df988be9051bda26a43a584b2a7
dgerike <dennis.gerike+github@gmail.com>
2020-09-11 18:18:53 +0200

    removing "ft lb" conversions ("lbf ft" is the correct unit)

--- src/Force.php
+++ src/Force.php
@@ -14,29 +14,11 @@ trait Force
     }
 
     /**
-     * @param $nm
-     * @return float
-     */
-    public static function nm2ftlb($nm): float
-    {
-        return self::nm2lbfft($nm);
-    }
-
-    /**
      * @param $lbfft
      * @return float
      */
     public static function lbfft2nm($lbfft): float
     {
         return $lbfft * 1.355818;
-    }
-
-    /**
-     * @param $lbfft
-     * @return float
-     */
-    public static function ftlb2nm($ftlb): float
-    {
-        return self::lbfft2nm($ftlb);
     }
 }
